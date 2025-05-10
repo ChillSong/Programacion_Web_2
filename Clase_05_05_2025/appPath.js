b3cbefef11cc5a3ca2e2c369f866a3e99d5e00c2
ChillSong <antonio.jair.garcia@hotmail.com>
2025-05-09 21:47:03 -0600

    actualizacion 09/05/2025

--- Clase_05_05_2025/appPath.js
+++ Clase_05_05_2025/appPath.js
@@ -8,12 +8,36 @@ const port = 3005;
 // Configurar la carpeta de archivos estáticos
 app.use(express.static(path.join(__dirname)));
 
-app.get("/",(req,res) =>{
-    //res.send("Hola mundo");
-    //res.sendFile("C:/Users/misae/Documents/web2/node/05052025/index.html");
-    res.sendFile(path.join(__dirname+"/index.html"));
+// Ruta principal
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, () =>{
+// Ruta 1
+app.get("/ruta1", (req, res) => {
+    res.send("<h1>Esta es la Ruta 1</h1>");
+});
+
+// Ruta 2
+app.get("/ruta2", (req, res) => {
+    res.send("<h1>Esta es la Ruta 2</h1>");
+});
+
+// Ruta 3
+app.get("/ruta3", (req, res) => {
+    res.send("<h1>Esta es la Ruta 3</h1>");
+});
+
+// Ruta 4
+app.get("/ruta4", (req, res) => {
+    res.send("<h1>Esta es la Ruta 4</h1>");
+});
+
+// Ruta 5
+app.get("/ruta5", (req, res) => {
+    res.send("<h1>Esta es la Ruta 5</h1>");
+});
+
+app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`);
 });
